f9c74c18a254d5d75e4dd756e9d9906f3ed90442
Ajitha-132006 <chalasaniajitha@gmail.com>
2025-12-20 19:09:58 +0530

    Update project with latest local changes

--- backend_test.py
+++ backend_test.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import time
 
 class LifeRPGAPITester:
-    def __init__(self, base_url="https://liferp.preview.emergentagent.com/api"):
+    def __init__(self, base_url="http://localhost:8000/api"):
         self.base_url = base_url
         self.token = None
         self.user_id = None
